d801bbd7a79d6d7330c1dda4c7801ad05edb6e0d
Kerstro <castrosituma@hotmail.com>
2026-06-06 15:52:48 -0500

    Ghanged the code to suit OOP

--- Grading_System.java
+++ Grading_System.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class Grading_System {
-    public static void main(String[] args) {
-        // Read input
+    public static int Inputter() {
         Scanner input = new Scanner(System.in);
-        int average;
+
         System.out.println("Enter your average mark");
+        int average = input.nextInt();
 
-        average = input.nextInt();
+        input.close();
 
-        // Grade now
+        return average;
+
+    }
+
+    // Grade now
+    public static void Grade(int average) {
         if (average >= 70) {
             System.out.println("Your Grade is A");
         } else if (average >= 60) {
@@ -19,7 +24,12 @@ public class Grading_System {
         } else if (average < 50) {
             System.out.println("Your Grade is FAIL");
         }
+    }
 
-        input.close();
+    public static void main(String[] args) {
+        // Run and display
+        int average = Inputter();
+        Grade(average);
+
     }
 }
